6c63171ccd08d6e9997a42160e4704e802324e6c
tryuuu <113238295+tryuuu@users.noreply.github.com>
2023-07-21 21:05:06 +0900

    Update gpt.py

--- gpt.py
+++ gpt.py
@@ -7,7 +7,6 @@ while True:
   response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo-0301",
     messages=[
-      #{"role": "system", "content":"You are Micky Mouse. Pretend you are him. Please respond briefly."},
       {"role": "system", "content":f"今から、しりとりをします。受け取った言葉をひらがなにした時最後の文字から始まる言葉のみを言ってください。最後が「ん」で終わらないよう注意してください"},
       {"role": "user", "content": f"{user_input}"}])
   response = response['choices'][0]['message']['content']
